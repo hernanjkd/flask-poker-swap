2771bfe7e0159462f0d3f6ba1c66f5b9275c82dd
Hernán García <hernanjkd@gmail.com>
2019-08-06 01:59:36 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -2,16 +2,43 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Person(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<Person %r>' % self.username
+        return f'<Person {self.username}>'
 
-    def serialize(self):
+    def serialize(self, admin=False):
+        if admin:
+            return {
+                "id": self.id,
+                "username": self.username,
+                "email": self.email
+            }
         return {
-            "username": self.username,
-            "email": self.email
+            "username": self.username
+        }
+
+
+class Tournament(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    players = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Tournament {self.name}>'
+
+    def serialize(self, admin=False):
+        if admin:
+            return {
+                "id": self.id,
+                "name": self.name,
+                "players": self.players
+            }
+        return {
+            "username": self.username
         }
